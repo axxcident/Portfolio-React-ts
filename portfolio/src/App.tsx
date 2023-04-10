128f7c46eb7fddba4ac063639958ffb03eb428a9
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-04-10 23:37:05 +0200

    en början

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,34 @@
 import React from "react";
 import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <nav className="navbar">
+        <div className="loggan">
+          <h2>A&O</h2>
+        </div>
+        <ul className="länkar">
+          <li>
+            <Link to={"/"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Portfolio</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Services</Link>
+          </li>
+          <li>
+            <Link to={"/"}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      {/* Allt i header borde vara i en pages/Home.tsx och det i sin tur innehålla components/PortfolioPage.tsx  */}
+      <Routes>
+        <Route path="/" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ portfolio/src/pages/About.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <header className="App-header">
+        <h1>Hello, my name is Axel Olivecrona</h1>
+        <h2>I'm a Web developer</h2>
+        <p>
+          I'm an aspiring Frontend engineer. Soon to be certified from
+          IT-Högskolan, Stockholm. I'm new to the field and I'd love to hear
+          about your website project you've been thinking about. I'm also
+          proficient with photoshop and design in general. Explore my portfolio
+          to see what projects I have been working on.
+        </p>
+      </header>
+    </>
+  );
+};
+
+export default About;
