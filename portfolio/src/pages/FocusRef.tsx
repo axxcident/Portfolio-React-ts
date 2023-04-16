b5fc6bddb8c2b9e834029c48f0dacbca71a00c25
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2023-04-16 22:57:49 +0200

    Savepoint innan nästa API grej

--- portfolio/src/pages/FocusRef.tsx
+++ portfolio/src/pages/FocusRef.tsx
@@ -3,10 +3,38 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { UserData } from "../models/types";
+import styled from "styled-components";
+import { GiMicrophone } from "react-icons/gi";
 
 interface FocusRefProps {
   bgTheme: boolean;
 }
+
+const FocusWrap = styled.div<{ bgTheme: boolean }>`
+  margin-left: 250px;
+  width: 75%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .focus-page img {
+    border-radius: 10px;
+  }
+  .question {
+    width: 50vw;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .question p {
+    font-size: 1.3rem;
+  }
+  #mikrofon {
+    font-size: 7rem;
+  }
+`;
 
 const FocusRef: React.FC<FocusRefProps> = ({ bgTheme }) => {
   const { id } = useParams<{ id: string }>(); // extract id parameter from URL path
@@ -26,16 +54,46 @@ const FocusRef: React.FC<FocusRefProps> = ({ bgTheme }) => {
 
   return (
     <>
-      <h5>sidan har laddats upp</h5>
-      {userData && (
-        <div>
-          <img src={userData.picture.large} alt="refperson" />
-          <p>
-            namn: {userData.name.title} {userData.name.first}{" "}
-            {userData.name.last}
-          </p>
-          <p>Email: {userData.email}</p>
-        </div>
+      {bgTheme ? (
+        <FocusWrap bgTheme={true}>
+          <div className="question">
+            <GiMicrophone id="mikrofon" />
+            <p>
+              In a short sentence, how would you describe working with Axel in
+              your latest project?
+            </p>
+          </div>
+          {userData && (
+            <div className="focus-page">
+              <img src={userData.picture.large} alt="refperson" />
+              <p>
+                namn: {userData.name.title} {userData.name.first}{" "}
+                {userData.name.last}
+              </p>
+              <p>Email: {userData.email}</p>
+            </div>
+          )}
+        </FocusWrap>
+      ) : (
+        <FocusWrap bgTheme={false}>
+          <div className="question">
+            <GiMicrophone id="mikrofon" />
+            <p>
+              In a short sentence, how would you describe working with Axel in
+              your latest project?
+            </p>
+          </div>
+          {userData && (
+            <div className="focus-page">
+              <img src={userData.picture.large} alt="refperson" />
+              <p>
+                namn: {userData.name.title} {userData.name.first}{" "}
+                {userData.name.last}
+              </p>
+              <p>Email: {userData.email}</p>
+            </div>
+          )}
+        </FocusWrap>
       )}
     </>
   );
